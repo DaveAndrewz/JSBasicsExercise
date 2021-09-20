10a45a4eb3772d58b6fd60dc002bd8252e61a011
Dave Andrewz <DaveAndrewz@hotmail.com>
2021-09-20 14:53:19 -0500

    Javascript Basics Exercise completed

---
+++ script.js
@@ -0,0 +1,28 @@
+console.log(13);
+
+// The five main data types are: Numbers, Strings, Boolean, Null & Undefined
+
+// The six operator symbols are '+' '-' '*' '/' '%' '**'
+
+console.log(5%4);
+
+console.log('I\'M MARY POPPINS, Y\'ALL!');
+
+console.log("Hawk" + "eye");
+
+console.log("Dave".length + "Andrew".length);
+
+console.log("Avengers: Endgame"[16]);
+
+console.log("\\");
+
+console.log(5**4 - 9);
+
+console.log("I".length + "Am".length + "Iron".length + "Man".length -10);
+
+console.table(["CAP"[1], "VISION"[0], "OKOYE"[4], "NICK FURY"[0], "GROOT"[0], "DOCTOR STRANGE"[13], "THOR"[3], "STAR-LORD"[0]]);
+
+const m=(25);
+const c=(4);
+let u = (m + c);
+console.log(u);
